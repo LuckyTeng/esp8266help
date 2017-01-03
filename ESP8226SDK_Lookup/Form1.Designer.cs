ab278a5adc7dd19e9c01ac7329fe984bc43d05cd
LuckyTeng <holy_necklace@yahoo.com.tw>
2017-01-03 17:42:59 +0700

    first commit

---
+++ ESP8226SDK_Lookup/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ESP8226SDK_Lookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && ( components != null ))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbKey = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbGrp = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            this.splitContainer1.Panel1MinSize = 255;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(765, 462);
+            this.splitContainer1.SplitterDistance = 255;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbKey);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(255, 359);
+            this.panel2.TabIndex = 2;
+            // 
+            // lbKey
+            // 
+            this.lbKey.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbKey.FormattingEnabled = true;
+            this.lbKey.ItemHeight = 12;
+            this.lbKey.Location = new System.Drawing.Point(12, 6);
+            this.lbKey.Name = "lbKey";
+            this.lbKey.Size = new System.Drawing.Size(227, 340);
+            this.lbKey.TabIndex = 3;
+            this.lbKey.SelectedIndexChanged += new System.EventHandler(this.lbKey_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbGrp);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtKey);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 103);
+            this.panel1.TabIndex = 1;
+            // 
+            // cbGrp
+            // 
+            this.cbGrp.FormattingEnabled = true;
+            this.cbGrp.Location = new System.Drawing.Point(14, 64);
+            this.cbGrp.Name = "cbGrp";
+            this.cbGrp.Size = new System.Drawing.Size(121, 20);
+            this.cbGrp.TabIndex = 3;
+            this.cbGrp.SelectedIndexChanged += new System.EventHandler(this.cbGrp_SelectedIndexChanged);
+            this.cbGrp.TextChanged += new System.EventHandler(this.cbGrp_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&group:";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(14, 24);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(143, 22);
+            this.txtKey.TabIndex = 1;
+            this.txtKey.TextChanged += new System.EventHandler(this.txtKey_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "key&word:";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(506, 462);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 462);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "ESP8266 SDK Quick Help";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lbKey;
+        private System.Windows.Forms.ComboBox cbGrp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ ESP8226SDK_Lookup/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+using System.Windows.Forms;
+
+namespace ESP8226SDK_Lookup
+{
+    public partial class Form1 :Form
+    {
+        private const string DOC_PATH = "docpath";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void txtKey_TextChanged(object sender, EventArgs e)
+        {
+            updateList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateGroup();
+            updateList();
+            //updateDoc("os_timer_arm");
+        }
+
+        private void updateList()
+        {
+            string keyword = txtKey.Text;
+            string group = cbGrp.Text;
+
+            // clear items
+            lbKey.Items.Clear();
+
+            XDocument doc = XDocument.Load("esp8266.xml");
+
+            if (keyword == String.Empty && group == String.Empty)
+            {
+                foreach (XElement result in doc.Elements("esp8266doc")
+                                               .Elements("keyword"))
+                    lbKey.Items.Add(result.Attribute("name").Value);
+            }
+            else
+            {
+                // filter by keyword
+                foreach (XElement result in doc.Elements("esp8266doc")
+                                               .Elements("keyword")
+                                               .Where(w => (keyword == String.Empty || w.Attribute("name").Value.Contains(keyword) )
+                                                    && ( group == String.Empty || w.Attribute("group").Value.Contains(group) ) ))
+                
+                    lbKey.Items.Add(result.Attribute("name").Value);
+            }
+        }
+
+        private void updateGroup()
+        {
+            cbGrp.Items.Clear();
+
+            XDocument doc = XDocument.Load("esp8266.xml");
+
+            foreach (XElement result in doc.Elements("esp8266doc")
+                                               .Elements("groups")
+                                               .Elements("group"))
+                cbGrp.Items.Add(result.Value);
+        }
+
+        private void updateDoc(string name)
+        {
+            webBrowser1.Url = null;
+
+            XDocument doc = XDocument.Load("esp8266.xml");
+
+            XElement result = doc.Elements("esp8266doc")
+                                 .Elements("keyword").FirstOrDefault(w => w.Attribute("name").Value == name);
+
+            if (result != null)
+            {
+                var xAttribute = result.Attribute(DOC_PATH);
+
+                string message;
+                if (xAttribute == null)
+                {
+                    // we try to use name to get file
+                    xAttribute  = result.Attribute("name");
+                    if (xAttribute == null) return;
+
+                    message = "docs\\" + xAttribute.Value + ".htm";
+                }
+                else
+                    message = xAttribute.Value;
+                var docfullpath = "file://" + System.IO.Directory.GetCurrentDirectory() + "\\" + message;
+                Debug.WriteLine(docfullpath);
+
+                webBrowser1.Url = new Uri(docfullpath);
+            }
+        }
+
+        private void lbKey_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var key = lbKey.Text;
+            updateDoc(key);
+        }
+
+        private void cbGrp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbGrp_TextUpdate(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbGrp_TextChanged(object sender, EventArgs e)
+        {
+            updateList();
+        }
+
+
+    }
+}
